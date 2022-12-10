b69dc32ae864703247de9a6983c4d1e1d1743608
romaboninineto <romeuboninigamer@gmail.com>
2022-12-10 03:28:34 -0300

    ARQ APAGADOS REST testado novamente

--- cursomc/src/main/java/com/dreamsites/cursomc/resources/CategoriaResource.java
+++ cursomc/src/main/java/com/dreamsites/cursomc/resources/CategoriaResource.java
@@ -5,14 +5,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
-	@RequestMapping(method=RequestMethod.GET)
+
+	@RequestMapping(method = RequestMethod.GET)
 	public String listar() {
-		
+
 		return "REST está funcionando!";
-		
+
 	}
 
 }
